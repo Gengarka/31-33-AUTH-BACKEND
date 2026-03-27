60bc62dba734a94ceda8fcc7c1ad7bb893727f51
Gengarka <gamesrock6@mail.ru>
2026-03-27 23:41:10 +0700

    + new coments

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,15 @@
+// импорт функций из модели пользователей
 import {
     getAllUsers as getAllUsersModel,
     findUserById,
 } from "../models/user.model.js";
 
+// сервис получения всех пользователей
 export async function getAllUsers() {
     return getAllUsersModel();
 }
 
+// сервис получения пользователя по id (опечатка: fundUserById вместо findUserById)
 export async function getUserById(id) {
     return fundUserById(id);
 }
