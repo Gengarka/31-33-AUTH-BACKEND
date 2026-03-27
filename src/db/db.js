d2c0d86bad4f173fad77e920fde3b16c3d89d6ff
Gengarka <gamesrock6@mail.ru>
2026-03-28 00:09:51 +0700

    fix

--- src/db/db.js
+++ src/db/db.js
@@ -26,14 +26,14 @@ db.exec(`
 
     CREATE TABLE IF NOT EXISTS refresh_tokens (
     id                   INTEGER PRIMARY KEY AUTOINCREMENT,     
-    user_id              INTEGER NOT NULL REFRENCES users(id) ON DELETE CASCADE,
+    user_id              INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     token_hash           TEXT NOT NULL UNIQUE,                 
     expires_at           TEXT NOT NULL,                   
     created_at           TEXT NOT NULL DEFAULT (datetime('now'))
 );
 
 CREATE INDEX IF NOT EXISTS idx_users_email ON users(email);                   
-CREATE INDEX IF NOT EXISTS idx_refresh_tokens-token_hash ON refresh_tokens(token_hash); 
+CREATE INDEX IF NOT EXISTS idx_refresh_tokens_token_hash ON refresh_tokens(token_hash);
 `);
 
 // Логирование успешной инициализации БД
